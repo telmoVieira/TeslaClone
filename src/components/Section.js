137d6a94bd8774c8d89fcb566b3679decef1f1d5
Telmo Vieira <telmo_vieira2@hotmail.com>
2021-05-17 17:22:29 +0100

    Add Buttons and Background

--- src/components/Section.js
+++ src/components/Section.js
@@ -4,7 +4,24 @@ import styled from "styled-components";
 function Section() {
     return (
         <Wrap>
-            
+            <ItemText>
+                <h1>Model S</h1>
+                <p>Order Online for Touchless Delivery</p>
+            </ItemText>
+            <ButtonGroup>
+                <LeftButton>
+                    Custom Order
+                </LeftButton>
+                <RightButton>
+                    Existing Inventory
+                </RightButton>
+            </ButtonGroup>
+            <DownArrow>
+
+            </DownArrow>
+
+
+
         </Wrap>
     )
 }
@@ -14,5 +31,40 @@ export default Section
 const Wrap = styled.div`
     width: 100vw;
     height: 100vh;
-    background: orange;
+    background-size: cover;
+    background-position: center;
+    background-repeat: not-repeat;
+    background-image: url('/img/model-s.jpg');
+    display: flex;
+    flex-direction: column;
+    justify-content: space-between; //vertical
+    align-items: center; //horizontal
+`
+const ItemText = styled.div`
+    padding-top: 15vh;
+    text-align: center;
+`
+const ButtonGroup = styled.div`
+    display: flex;
+    margin-bottom: 30px;
+`
+const LeftButton = styled.div`
+    background-color: rgba(23, 26, 32, 0.8);
+    height: 40px;
+    width: 256px;
+    color: white;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    border-radius: 100px;
+    opacity: 0.85;
+    text-transform: uppercase;
+    font-size: 12px;
+    cursor: pointer;
+`
+const RightButton = styled(LeftButton)`
+
+`
+const DownArrow = styled(LeftButton)`
+
 `
