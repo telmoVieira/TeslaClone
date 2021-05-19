7ec1cfee57f8346210fcb20a6f962d0c8a89e101
Telmo Vieira <telmo_vieira2@hotmail.com>
2021-05-19 11:13:01 +0100

    Add Fade to Tittles

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,11 +1,80 @@
 import React from 'react'
+import styled from "styled-components"
+import MenuIcon from '@material-ui/icons/Menu';
 
 function Header() {
     return (
-        <div>
-            header
-        </div>
+        <Container>
+            <a>
+                <img src="/img/logo.svg" alt="" />
+            </a>
+            <Menu>
+                <a href="#">Model S</a>
+                <a href="#">Model 3</a>
+                <a href="#">Model X</a>
+                <a href="#">Model Y</a>
+            </Menu>
+            <RightMenu>
+                <a href="#">Shop</a>
+                <a href="#">Tesla Account</a>
+                <CustomMenu />
+            </RightMenu>
+            <BurgerNav>
+                <li><a href="#">Existing Iventory</a></li>
+                <li><a href="#">Used Iventory</a></li>
+                <li><a href="#">Trade</a></li>
+                <li><a href="#">Cybertruck</a></li>
+                <li><a href="#">Roadaster</a></li>
+                <li><a href="#">Existing Iventory</a></li>
+                <li><a href="#">Existing Iventory</a></li>
+                <li><a href="#">Existing Iventory</a></li>
+            </BurgerNav>
+        </Container>
     )
 }
 
 export default Header
+
+const Container = styled.div`
+    min-height: 60px;
+    position: fixed;
+    display: flex;
+    align-items: center;
+    justify-content: space-between;
+    padding: 0 20px;
+    top: 0;
+    left: 0;
+    right: 0;
+`
+
+const Menu = styled.div`
+    display: flex;
+    align-items: center;
+    justify-content: center;
+    flex: 1;
+    font-size: 20px;
+
+    a {
+        font-widht: 600;
+        text-transform: uppercase;
+        padding: 0 10px;
+        flex-wrap: nowrap;
+    }
+
+    @media(max-width: 768px) {
+        display: none;
+    }
+
+`
+const RightMenu = styled.div`
+    display: flex;
+    align-items: center;
+    a {
+        font-widht: 600;
+        text-transform: uppercase;
+        margin-right: 10px;
+    }
+`
+const CustomMenu = styled(MenuIcon)`
+    cursor: pointer;
+`
--- src/components/Section.js
+++ src/components/Section.js
@@ -1,24 +1,29 @@
 import React from 'react'
 import styled from "styled-components";
+import Fade from 'react-reveal/Fade';
 
 function Section({ title, description, leftBtnText, rightBtnText, backgroundImg }) {
     return (
         <Wrap bgImage={backgroundImg}>
-            <ItemText>
-                <h1>{ title }</h1>
-                <p>{ description }</p>
-            </ItemText>
+            <Fade bottom>
+                <ItemText>
+                    <h1>{title}</h1>
+                    <p>{description}</p>
+                </ItemText>
+            </Fade>
             <Buttons>
-                <ButtonGroup>
-                    <LeftButton>
-                        { leftBtnText }
-                    </LeftButton>
-                    { rightBtnText && 
-                    <RightButton>
-                        { rightBtnText }
-                    </RightButton>
-                    }
-                </ButtonGroup>
+                <Fade bottom>
+                    <ButtonGroup>
+                        <LeftButton>
+                            {leftBtnText}
+                        </LeftButton>
+                        {rightBtnText &&
+                            <RightButton>
+                                {rightBtnText}
+                            </RightButton>
+                        }
+                    </ButtonGroup>
+                </Fade>
                 <DownArrow src="/img/down-arrow.svg" />
             </Buttons>
         </Wrap>
@@ -38,7 +43,7 @@ const Wrap = styled.div`
     flex-direction: column;
     justify-content: space-between; //vertical
     align-items: center; //horizontal
-    background-image: ${props => `url("/img/${props.bgImage}")` }
+    background-image: ${props => `url("/img/${props.bgImage}")`}
 `
 const ItemText = styled.div`
     padding-top: 15vh;
